865a24ccf025cf054ca644948bb77006595db476
Raphael Rumin <rmr4685a@ens.fsi>
2023-04-20 17:45:48 +0200

    EX_2

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -3,6 +3,7 @@ package ilu2;
 public class Welcome {
 	
 	public static String welcome(String input) {
+		if (input == null || input.trim().isEmpty()) return "Hello, my friend";
 		StringBuilder chaine = new StringBuilder();
 		chaine.append("Hello, ");
 		input = input.substring(0, 1).toUpperCase() + input.substring(1);
--- src/ilu2/test/WelcomeTest.java
+++ src/ilu2/test/WelcomeTest.java
@@ -12,5 +12,12 @@ class WelcomeTest {
 		assertEquals(Welcome.welcome("bob"),"Hello, Bob");
 		assertEquals(Welcome.welcome("rasa"),"Hello, Rasa");
 	}
+	
+	@Test
+	void ex2() {
+		assertEquals(Welcome.welcome("   "),"Hello, my friend");
+		assertEquals(Welcome.welcome(""),"Hello, my friend");
+		assertEquals(Welcome.welcome(null),"Hello, my friend");
+	}
 
 }
